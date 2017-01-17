2f5fb96b4e82a568b93f8d469c6fe528262bd947
dongliang.ma <dongliang.ma@baozun.cn>
2017-01-17 14:10:56 +0800

    单点应用-服务端2-api

---
+++ src/main/java/com/discovery/darchrow/sec/model/Item.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2015 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.discovery.darchrow.sec.model;
+
+/**
+ * 商品信息.
+ * 
+ */
+public class Item{
+	
+	/** Id. */
+	private Long				id;
+	
+	/** 店铺Id. */
+	private Long				storeId;
+	
+	/** 商品编码 */
+	private String 				code;
+
+	/** 商品标题. */
+	private String				title;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(Long storeId) {
+		this.storeId = storeId;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
+}
